6c52a6d13a8b5a016ca9ea4999d8c10774e843d8
Alexis Puga Ruíz <alexispuga@pm.me>
2019-08-31 16:53:06 -0500

    BUG: Fix "webpack bundles not found in docs"
    Add a plugin to copy files.
    On production, copy bundles to "docs/demo" directory.
    
    CAUSE:
    
    Webpack bundles weren't being saved in the docs directory for production
    mode, so "npm run build" wasn't working properly.
    
    For development mode that wasn't the case, since the default directory
    for serving bundles is in that path.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,23 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'source-map'
+    devtool: 'source-map',
+    plugins: [
+        new CopyPlugin([
+            {
+                from: 'static/js/main*',
+                to: path.resolve('docs/demo/js/'),
+                flatten: true
+            },
+            {
+                from: 'static/css/main*',
+                to: path.resolve('docs/demo/css/'),
+                flatten: true
+            }
+        ])
+    ]
 });
