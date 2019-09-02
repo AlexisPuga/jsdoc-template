595056ca5a4db39474efabbdc3ff85c0ca8df3a1
Alexis Puga Ruíz <alexispuga@pm.me>
2019-09-02 02:27:38 -0500

    BUG: Copying builds to "docs/demo" still not working
    PROBLEM: CopyWebpackPlugin doesn't get called after compilation is done.
    
    SOLUTION: Detect when webpack compilation is done and copy files using
    NodeJs to avoid window/linux/mac problems.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,39 @@
-const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const CopyPlugin = require('copy-webpack-plugin');
+const copy = require('copy');
+const AfterBuildPlugin = class AfterBuildWebpackPlugin {
+    constructor(callback) {
+        this.callback = callback;
+    }
+    apply(compiler) {
+        compiler.hooks.done.tapPromise('AfterBuildWebpackPlugin',
+            (...args) => this.callback.apply(compiler, args));
+    }
+};
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
     plugins: [
-        new CopyPlugin([
-            {
-                from: 'static/js/main*',
-                to: path.resolve('docs/demo/js/'),
-                flatten: true
-            },
-            {
-                from: 'static/css/main*',
-                to: path.resolve('docs/demo/css/'),
-                flatten: true
-            }
-        ])
+        new AfterBuildPlugin(function copyBundles() {
+            const from = ['static/js/main*', 'static/css/main*'];
+            const to = ['docs/demo/js', 'docs/demo/css'];
+
+            return Promise.all(from.map((src, i) => {
+                const dest = to[i];
+
+                return new Promise((resolve, reject) => {
+                    copy(src, dest, (error, files) => {
+                        if (error) {
+                            return reject(error);
+                        }
+
+                        return resolve(files);
+                    });
+                });
+            })).catch(error => {
+                console.log(error);
+            });
+        })
     ]
 });
