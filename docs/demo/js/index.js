11be3f4975be28f57b6dae3844a70d6d4a134953
Alexis Puga Ruíz <alexispuga@pm.me>
2019-09-23 02:56:45 -0500

    Revert "BUG: Ignore "docs/demo" and "main" files"
    This reverts commit a77dfc77a278921c642173a5110c398c99dbb723.

---
+++ docs/demo/js/index.js
@@ -0,0 +1,31 @@
+import '../css/jsdoc.css';
+import '../css/views/index.css';
+import './states';
+import './buttonHandlers';
+import './search';
+import './linenumber';
+
+(function() {
+    var helpers = require('./helpers');
+    var getCurrentPageID = function() {
+        return location.href.split(/.+\//)[1];
+    };
+    var updateActiveLink = function() {
+        var currentPageID = getCurrentPageID();
+
+        helpers.find('nav li > a[href="' + currentPageID + '"]').forEach(function(link) {
+            helpers.addState(link.parentNode);
+        });
+    };
+
+    helpers.on(window, 'hashchange', function() {
+        helpers.find('#page-header-opener')[0].click();
+        helpers.find('nav li.is-active > a').forEach(function(target) {
+            helpers.removeState(target.parentNode);
+        });
+
+        updateActiveLink();
+    });
+
+    updateActiveLink();
+})();
