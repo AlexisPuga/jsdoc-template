d3092bb9d316dde3d74d85fb369a3f9181a4e628
Alexis Puga Ruíz <alexispuga@pm.me>
2019-09-28 11:31:09 -0500

    BLD: Run "npm run build"

--- docs/demo/js/index.js
+++ docs/demo/js/index.js
@@ -19,12 +19,20 @@ import './linenumber';
     };
 
     helpers.on(window, 'hashchange', function() {
-        helpers.find('#page-header-opener')[0].click();
         helpers.find('nav li.is-active > a').forEach(function(target) {
             helpers.removeState(target.parentNode);
         });
 
         updateActiveLink();
+    });
+
+    helpers.on('a', 'click', function() {
+        var pageHeaderOpener = helpers.find('#page-header-opener')[0];
+
+        /** Close nav when an anchor is clicked. */
+        if (this.hash && helpers.hasState(pageHeaderOpener, 'is-active')) {
+            pageHeaderOpener.click();
+        }
     });
 
     updateActiveLink();
